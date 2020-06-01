9b3a688a7cc7605a6ff539523b49803733b6f98e
kaur9110 <kaur9110@sheridancollege.ca>
2020-06-01 01:13:17 -0500

    First code

--- src/examplewednesday/ExampleWednesday.java
+++ src/examplewednesday/ExampleWednesday.java
@@ -15,7 +15,7 @@ public class ExampleWednesday {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello worlds");
         // TODO code application logic here
     }
     
